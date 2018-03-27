796ef9ef3d8230fc61e358a81a2a6035e37ab938
Spencer Manzon <samanzon@ucalgary.ca>
2018-03-27 16:45:00 -0600

    proto database

--- app/io.js
+++ app/io.js
@@ -6,12 +6,20 @@ io.connection = function(socket) {
         
     
     console.log(':::socketio::: user: ', socket.request.user);
-    console.log(userdb.find({username: socket.request.user.username}));
     /*
     if (socket.request.user && socket.request.user.logged_in) {
         console.log(socket.request.user);
     }
     */
+    userdb.findOne({username: socket.request.user.username}, function(err, doc) {
+
+        if (err) {
+            console.log('error occured', err);
+        }
+        else {
+            console.log('the document', doc);
+        }
+    });
     let info = {username: socket.request.user.username, isLoggedIn: socket.request.user.logged_in};
     socket.emit('init', info);
 };
