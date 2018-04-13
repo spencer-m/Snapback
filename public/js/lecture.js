3301dac7e1640989c68c07e1db3d911342e4cfb4
Nathan Dien <nathandien@gmail.com>
2018-04-13 15:30:17 -0600

    Started working on dynamically loading lecture pages
    -Created new function in lecture.js for dynamically loading lecture pages
    -Commented out upload button (for now, will move later)
    -Removed some unnecessary commented code

--- public/js/lecture.js
+++ public/js/lecture.js
@@ -8,63 +8,51 @@ function File(section, filePath, fileName) {
   this.section = section;
   this.filePath = filePath;
   this.fileName = fileName;
-  
+
 
   this.view = function view() {
-	/*
-	// Creates the basic card (section)
-	var card = document.createElement("div");
-	card.className = "card";
 
-	// Creates the card-header
-	var header = document.createElement("div");
-	header.className = "card-header";
-	header.innerHTML = section;
-
-	// Creates the card-link for the section name
-	var link = document.createElement("a");
-	link.className = "card-link";
-	link.setAttribute("data-toggle", "collapse");
-	link.setAttribute("href", "#collapse-" + section);
-	link.innerHTML = section;
-	*/
-	var co = "body-" + section;
-	console.log(co);
 	var content = document.getElementById("list-" + section);
 
-	// Creates the card-body for storing files
-	//var fileList = document.createElement("ul");
-	//fileList.className = "list-group list-group-flush";
-
-	// Gets all the files and appends to fileList (card-body)
-	//files.forEach(file => {
 	var listElement = document.createElement("li");
 	listElement.className = "list-group-item";
 	var link = document.createElement("a");
 	link.setAttribute("href", filePath);
 	link.setAttribute("download", fileName);
-	
+
 	link.innerHTML = fileName;
-	
+
 	console.log(link);
-	
+
 	listElement.append(link);
-	//fileList.append(listElement);
-	//});
 
 	content.append(listElement);
 
-	/*
-	card.append(header);
-	card.append(link);
-
-	// Appends new section and card to accordion
-	document.getElementsById("accordion").append(card);
-	*/
   }
 
 }
-	
+
+function lectureStart(lectureName) {
+
+
+  this.load = function load() {
+    var mainNavBar = document.createElement("nav");
+    mainNavBar.className = "navbar navbar-default";
+
+    var container = document.createElement("div");
+    container.className = "container-fluid";
+
+    var navHeader = document.createElement("div");
+    navHeader.className = "navbar-header";
+
+    var headerTitle = document.create("div");
+    headerTitle.className = "header-title";
+    headerTitle.innerHTML = lectureName;
+
+
+  }
+}
+
 
 var file = new File("lecture", "testFile.txt", "testFile");
 var file2 = new File("lecture", "testFile.txt", "testFile2");
