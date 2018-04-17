c08cecbb9a8840649b24634640f4057f925ecfd8
Kouroshb26 <kourosh.banaeianzade@ucalgary.ca>
2018-04-16 21:29:53 -0600

    Finalized Add Session
    Add Session now works

--- public/js/questions.js
+++ public/js/questions.js
@@ -55,7 +55,8 @@ function session(_id,isLive,name,questions){
         if(isLive){
             card.addClass("live");
         }
-        return (card);
+
+        return ( $(`<div class="col-md-12 col-lg-4">`).append(card));
     }
 }
 
@@ -365,52 +366,65 @@ function sessionsView(regcode){
                             .text("Add Session")
                             .attr("type","button")
                             .attr("data-toggle","modal")
-                            .attr("data-target","#add-session-modal")));
+                            .attr("data-target","#add-session-modal")
+                            .click(function(){
+                                $("#question-modal-content").empty();
+                                $(".modal-content").append($(`
+                                <div class="modal-header">
+                                    <h3 class="modal-title">Make New Session</h3>
+                                </div>
+                                <div class="modal-body">
+                                    <div>
+                                        <p>Please Enter Session Name</p>
+                                        <input class="form-control" id="session-name" placeholder="eg. Session #1">
+                                    </div>
+                                    <div>
+                                        <p>Make session live?</p>
+                                        <select class="form-control" id="session-isLive" name="semesters" form="semesterForm">
+                                        <option value="True">True</option>
+                                        <option value="False">False</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div id ="question-modal-footer" class="modal-footer">                                   
+                                </div>`));
+                                let submit = $(`<button type="button" id="session-submit-button" class="btn btn-blue">Submit</button>`).click(function(){
+                                    
+                                    let newSession = new session(null,$("#session-isLive").val() === "True",$('#session-name').val(),[]);
+                                    socket.emit("addSession",courseID._id,newSession,function(state){
+                                        if(state ==="success"){
+                                            $("#question-modal-content").empty();
+                                            $("#question-modal-content").append($(`
+                                                <div class="modal-header">
+                                                    <h3 class="modal-title">Success!</h3>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <div>
+                                                        <p>Making the session was a success</p>
+                                                    </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" id="session-cancel-button" class="btn btn-blue" data-dismiss="modal">Close</button>
+                                                </div>
+                                            `));
+                                        }
+                                    });
+                             
+                                });
+                                let close = $(`<button type="button" id="session-cancel-button" class="btn btn-blue" data-dismiss="modal">Close</button>`);
+                                $("#question-modal-footer").append(submit).append(close);
+                            })));
 
     $(".sessions-list").append($(`
     <div class="modal fade" id="add-session-modal">
         <div class="modal-dialog h-100 d-flex flex-column justify-content-center my-0">
-
-            <div class="modal-content">
-
-                <div class="modal-header">
-                <h3 class="modal-title">Make New Session</h3>
-                </div>
-
-                <div class="modal-body">
-                <div>
-                    <p>Please Enter Session Name</p>
-                    <input class="form-control" id="session-name" placeholder="eg. Session #1">
-                </div>
-                <div>
-                    <p>Make session live?</p>
-                    <select class="form-control" id="session-isLive" name="semesters" form="semesterForm">
-                    <option value="true">true</option>
-                    <option value="false">false</option>
-                    </select>
-                </div>
-                </div>
-
-                <div class="modal-footer">
-                <button type="button" id="session-submit-button" class="btn btn-blue">Submit</button>
-                <button type="button" id="session-cancel-button" class="btn btn-blue" data-dismiss="modal">Close</button>
-                </div>
+            <div id ="question-modal-content" class="modal-content">
             </div>
         </div>
     </div>`));
 
-    $("#session-submit-button").click(function(){
-        let newSession = new session(null,$("#session-isLive").val(),$('#session-name').val(),[]);
-        socket.emit("addSession",courseID._id,session,function(state){
-            if(state ==="succes"){
-
-            }else{
-
-            }
-        });
- 
-    })
     socket.emit("loadClass",savedregcode,function(userinfo,classinfo){
+        console.log(classinfo);
         client = userinfo;
         courseID = classinfo;
         sessions = [];
@@ -421,7 +435,7 @@ function sessionsView(regcode){
         }
 
         sessions.forEach(session =>{
-            $(".sessions-list").append($(`<div class="col-md-12 col-lg-4">`).append(session.view()));
+            $(".sessions-list").append(session.view());
         })
     })
 }
@@ -432,9 +446,11 @@ $(document).ready(function(){
     socket.on("addedQuestion",function(session_id,addQuestion){
         console.log(addQuestion._id);
         if(session_id == clientSession._id){
-            let newQuestion = new question(addQuestion._id,addQuestion.question,addQuestion.author,addQuestion.date,addQuestion.upvotes,addQuestion.downvotes,addQuestion.comments)
-            clientQuestions.push(newQuestion);
-            $(".questions-list").append(newQuestion.view());
+            if($('.sessions-list').is(':empty')){
+                let newQuestion = new question(addQuestion._id,addQuestion.question,addQuestion.author,addQuestion.date,addQuestion.upvotes,addQuestion.downvotes,addQuestion.comments)
+                clientQuestions.push(newQuestion);
+                $(".questions-list").append(newQuestion.view());
+            }
         }
     });
 
@@ -500,7 +516,20 @@ $(document).ready(function(){
         }
 
     });
-    sessionsView("RW91C3");
+
+    socket.on("addedSession",function(course_id,addSession){
+        if(courseID._id = course_id){
+            if($('.questions-list').is(':empty')){
+                let newSession = new session(addSession._id,addSession.isLive,addSession.name,addSession.questions);
+                sessions.push(newSession);
+                $(".sessions-list").append(newSession.view());
+            }
+           
+
+        }
+
+    });
+    sessionsView("DT9OAY");
 
 });
 
